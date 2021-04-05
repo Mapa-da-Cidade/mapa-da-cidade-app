e8b520529e3f6ce6318398a64adffdb5f0918c84
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-04-05 19:49:21 -0300

    renomeando tab1

--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -2,10 +2,10 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
+import { FeedPage } from './feed.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { FeedPageRoutingModule } from './feed-routing.module';
 
 @NgModule({
   imports: [
@@ -13,8 +13,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    FeedPageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [FeedPage]
 })
-export class Tab1PageModule {}
+export class FeedPageModule { }
