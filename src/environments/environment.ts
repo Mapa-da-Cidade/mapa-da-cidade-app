a02bdea0473fef149ca37a952968e508e942e153
DevLucasOliveira <lucasoliveira.si@outlook.com>
2021-04-05 22:41:13 -0300

    Adicionando configs firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBPxF76MU82coQmAETidL_r43Fde493x8w",
+    authDomain: "mapa-da-cidade-app.firebaseapp.com",
+    projectId: "mapa-da-cidade-app",
+    storageBucket: "mapa-da-cidade-app.appspot.com",
+    messagingSenderId: "706298410812",
+    appId: "1:706298410812:web:22bb2e628d45618e761039"
+  }
 };
 
 /*
