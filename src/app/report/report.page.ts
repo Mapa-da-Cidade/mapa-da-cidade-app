a83128fbbac2d8c241247ff482a5b52fc79026c8
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-04-05 22:13:33 -0300

    colocando imagens

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -12,11 +12,11 @@ export class ReportPage {
 
   constructor() {
     this.reports = new Array<Report>(
-      new Report('', 'Árvores em risco', 'Sinalizar uma árvore que precisa de poda'),
-      new Report('', 'Áreas de alagamento', 'Sinalizar pontos de enchente'),
-      new Report('', 'Roubo / Furto', 'Sinalizar áreas com alto índice de roubos e furtos'),
-      new Report('', 'Vias irregulares', 'Sinalizar ruas com buracos'),
-      new Report('', 'Falta de Iluminação', 'Áreas sem iluminação'),
+      new Report('assets/images/tree-leaf.svg', 'Árvores em risco', 'Sinalizar uma árvore que precisa de poda'),
+      new Report('assets/images/boat.svg', 'Áreas de alagamento', 'Sinalizar pontos de enchente'),
+      new Report('assets/images/sad.svg', 'Roubo / Furto', 'Sinalizar áreas com alto índice de roubos e furtos'),
+      new Report('assets/images/flag.svg', 'Vias irregulares', 'Sinalizar ruas com buracos'),
+      new Report('assets/images/flashlight.svg', 'Falta de Iluminação', 'Áreas sem iluminação'),
     );
   }
 
