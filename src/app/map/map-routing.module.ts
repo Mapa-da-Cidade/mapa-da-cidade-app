6214630d071f1026b3a17805483f2a654397e97b
Lucas Souza <lucas.souza@idwall.co>
2021-04-09 21:28:53 -0300

    feat: ao abrir a camera, abrir na posição do usuário

--- src/app/map/map-routing.module.ts
+++ src/app/map/map-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MapPage } from './map.page';
+import {Geolocation} from "@ionic-native/geolocation/ngx";
 
 const routes: Routes = [
   {
@@ -11,6 +12,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    Geolocation
+  ]
 })
 export class MapPageRoutingModule { }
--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,8 +1,9 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Platform} from '@ionic/angular';
-import {Environment, GoogleMap, GoogleMapOptions, GoogleMaps} from '@ionic-native/google-maps';
+import {Environment, GoogleMap, GoogleMapOptions, GoogleMaps, GoogleMapsEvent} from '@ionic-native/google-maps';
 import {mapStyle} from './map-style';
 import {ReportService} from '../shared/services/report.service';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-map',
@@ -15,7 +16,26 @@ export class MapPage implements OnInit {
 
   map: GoogleMap;
 
-  constructor(private platform: Platform, private reportService: ReportService) {
+  constructor(private platform: Platform,
+              private reportService: ReportService,
+              private geolocation: Geolocation) {
+    this.geolocation.getCurrentPosition().then(location => {
+
+      const mapOptions: GoogleMapOptions = {
+        camera: {
+          target: {
+            lat: location.coords.latitude,
+            lng: location.coords.longitude
+          }, zoom: 18, tilt: 30,
+        },
+        styles: mapStyle,
+        preferences: {zoom: {minZoom: 6, maxZoom: 20}}, controls: {myLocation: true}
+      };
+      this.map = GoogleMaps.create('map_canvas', mapOptions);
+      this.map.setMyLocationEnabled(true);
+
+    });
+
   }
 
   async ngOnInit() {
@@ -25,42 +45,24 @@ export class MapPage implements OnInit {
   }
 
   loadMap() {
+    this.generateMarkers();
 
     Environment.setEnv({
       API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyD_RA628bGTIXcJ9WX5jjGjFelHEko3QeI',
       API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyD_RA628bGTIXcJ9WX5jjGjFelHEko3QeI'
     });
 
-    const mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: -23.5588365,
-          lng: -46.6610601
-        },
-        zoom: 18,
-        tilt: 30
-      },
-      styles: mapStyle
-    };
-
-    this.map = GoogleMaps.create('map_canvas', mapOptions);
-
-    this.generateMarkers();
-
-    // marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-    //   alert('clicked');
-    // });
   }
 
   generateMarkers() {
     this.reportService.getReports().subscribe((reports) => {
       console.log(reports);
       this.map.clear().then(() => {
-        reports.map(mockedReport => {
+        reports.map(report => {
           return {
             title: '',
             icon: {
-              url: mockedReport.icon,
+              url: report.icon,
               size: {
                 width: 45,
                 height: 45
@@ -68,19 +70,22 @@ export class MapPage implements OnInit {
             },
             snippet: `
                 <div class="marker-container">
-                  <img src="${mockedReport.photo}">
-                  <h4 class="text-uppercase">${mockedReport.title}</h4>
+                  <img src="${report.photo}">
+                  <h4 class="text-uppercase">${report.title}</h4>
                   <small class="text-uppercase">Status</small>
-                  <label style="font-size: 15px">${mockedReport.status}</label>
+                  <label style="font-size: 15px">${report.status}</label>
                 </div>`,
             animation: 'DROP',
             position: {
-              lat: mockedReport.location.latitude,
-              lng: mockedReport.location.longitude
+              lat: report.location.latitude,
+              lng: report.location.longitude
             }
           };
-        }).forEach(marker => {
-          this.map.addMarkerSync(marker);
+        }).forEach(markerReport => {
+          const createdMarker = this.map.addMarkerSync(markerReport);
+          createdMarker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+            // openReport(markerReport);
+          });
         });
       });
     });
